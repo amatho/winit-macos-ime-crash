cf9c139fc22fee2fe214d1c0f2855affb86efb49
Amandus Søve Thorsrud <ama.thorsrud@gmail.com>
2023-04-03 18:00:12 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use std::{sync::Arc, thread, time::Duration};
+
+use winit::{
+    dpi::LogicalPosition,
+    event::{Event, WindowEvent},
+    event_loop::EventLoop,
+    window::WindowBuilder,
+};
+
+fn main() {
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+    let window = Arc::new(window);
+
+    thread::spawn(move || {
+        // Wait for event loop to initialize
+        thread::sleep(Duration::from_secs(1));
+        window.set_ime_position(LogicalPosition::new(42, 42));
+    });
+
+    event_loop.run(move |event, _, control_flow| {
+        control_flow.set_wait();
+
+        if let Event::WindowEvent {
+            event: WindowEvent::CloseRequested,
+            ..
+        } = event
+        {
+            control_flow.set_exit();
+        }
+    });
+}
